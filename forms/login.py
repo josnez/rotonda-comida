4283fc8d69968c7638c2c540e81539db5a50d00f
josnnez <josue.nunez@mercadolibre.com.co>
2023-05-22 13:16:03 -0500

    add gitignore file

--- forms/login.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SearchField, SubmitField
-
-class LogIn(FlaskForm):
-    usuario = StringField("usuario")
-    enviar = SubmitField("enviar")
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clave'
 
+""" Database configurations """
+directory = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(directory, 'rotondacomidas.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+api = Api(app)
+
 @app.route("/")
 def init():
-    return render_template('portada.html')
+    return "render_template('portada.html')"
+
+@app.route("/login")
+def login():
+    return "render_template('logIn.html')"
 
 @app.route("/registro/restaurante")
 def registro_restaurante():
-    return render_template('registroRestaurante.html')
+    return "render_template('registroRestaurante.html')"
 
 @app.route("/registro/producto")
 def registro_producto():
